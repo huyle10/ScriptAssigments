14f8e2f55038a9a7901068ac55285a2775f33bc1
Huy Le <huyle10@outlook.com>
2017-12-23 22:21:57 -0600

    Update regexPhone.py

--- Python/regexPhone.py
+++ Python/regexPhone.py
@@ -1,5 +1,5 @@
 # Extract phone number in a string
 import re
 
-phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d)
+phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
 print(phoneNumRegex.finall('Call me 415-555-1011 tomorrow, or 415-555-999'))
