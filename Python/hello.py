56b1b8e54ca61a2f2dd3f83b806a98fcea309a7f
Huy Le <huyle10@outlook.com>
2017-12-23 21:38:37 -0600

    Create hello.py

---
+++ Python/hello.py
@@ -0,0 +1,9 @@
+#!/bin/python
+
+# To use scripts as command, change file name to hello and EXPORT PATH=~/<current folder path>:$PATH
+first_name = "Huy"
+last_name = "Le"
+age = xx
+birth_date = "mm/dd/yyyy"
+
+print("My name is %s %s.\nI was born on %s and I'm %s years old." % (first_name, last_name, birth_date, age))
